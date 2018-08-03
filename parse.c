656c729ae45b8fef0358ab6f801cff2b7e0e343b
Jean-Francois Moine <moinejf@free.fr>
2018-08-03 11:24:52 +0200

    fix: bad transposition when chord symbols separated by ';'
    Reported by Peter Yarensky.

--- parse.c
+++ parse.c
@@ -1425,28 +1425,38 @@ static void gch_capo(struct SYMBOL *s)
 
 static void gch_transpose(struct SYMBOL *s)
 {
+	int in_ch = 0;
 	int i2 = curvoice->ckey.sf - curvoice->okey.sf;
-	char *o = s->text, *p = o, *q = o, *r;
+	char *o = s->text, *p = o;
 
 	// search the chord symbols
 	for (;;) {
-		if (!strchr("^_<>@", *p)) {
-			q = strchr(p, '\t');
-			if (q) {
-				r = strchr(p, '\n');
-				if (!r || q < r) {
-					gch_tr1(s, q + 1 - o, i2);
-					p = s->text + (p - o);
-					o = s->text;
-				}
-			}
+		if (in_ch || !strchr("^_<>@", *p)) {
 			gch_tr1(s, p - s->text, i2);
 			p = s->text + (p - o);
 			o = s->text;
+			for (p++; *p; p++) {
+				if (strchr("\t;\n", *p))
+					break;
+			}
+			if (!*p)
+				break;
+			switch (*p) {
+			case '\t':
+				in_ch = 1;
+				break;
+			case ';':
+				in_ch = !strchr("^_<>@", p[1]);
+				break;
+			default:
+				in_ch = 0;
+				break;
+			}
+		} else {
+			p = strchr(p, '\n');
+			if (!p)
+				break;
 		}
-		p = strchr(p, '\n');
-		if (!p)
-			return;
 		p++;
 	}
 }

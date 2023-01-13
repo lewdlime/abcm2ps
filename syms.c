lewdlime/abcm2ps

commit e3282f8fa609518defa241a528f447d8351a55c6

move the single bars 1 point to the left
Issue #109.

syms.c
@@ -374,7 +374,7 @@ static char ps_head[] =
 
 	/* -- bars -- */
 	/* h x y bar - thin bar */
-	"/bar{M 1 SLW 0 exch RL stroke}!\n"
+	"/bar{3 -1 roll 1 exch rectfill}!\n"
 	/* h x y dotbar - dotted bar */
 	"/dotbar{[5] 0 setdash bar [] 0 setdash}!\n"
 	/* h x y thbar - thick bar */

draw.c
@@ -1244,6 +1244,7 @@ static void draw_bar(struct SYMBOL *s, float bot, float h)
 		case B_BAR:
 			if (s->u.bar.dotted)
 				psf = "dotbar";
+			x -= 1;
 			break;
 		case B_OBRA:
 		case B_CBRA:

commit 0705790a2caf53362afa7b2890b4add15fa5483f

fix: dashed bars broken in PostScript output since commit e3282f8f
Reported by Paul Morrissett.

syms.c
@@ -376,7 +376,7 @@ static char ps_head[] =
 	/* h x y bar - thin bar */
 	"/bar{3 -1 roll 1 exch rectfill}!\n"
 	/* h x y dotbar - dotted bar */
-	"/dotbar{[5] 0 setdash bar [] 0 setdash}!\n"
+	"/dotbar{M 1 SLW 0 exch RL [5] 0 setdash stroke [] 0 setdash}!\n"
 	/* h x y thbar - thick bar */
 	"/thbar{3 -1 roll 3 exch rectfill}!\n"
 	/* x y rdots - repeat dots */